d254d1b05a72aba4cc5634dbf067d687b39a21fe
LuntikLaElite <saneksivij@yandex.ru>
2022-11-07 20:30:00 +0400

    Добавьте файлы проекта.

---
+++ capcha/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace capcha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pBox = new System.Windows.Forms.PictureBox();
+            this.tb = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnRestart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pBox
+            // 
+            this.pBox.Location = new System.Drawing.Point(12, 12);
+            this.pBox.Name = "pBox";
+            this.pBox.Size = new System.Drawing.Size(169, 63);
+            this.pBox.TabIndex = 0;
+            this.pBox.TabStop = false;
+            // 
+            // tb
+            // 
+            this.tb.Location = new System.Drawing.Point(196, 36);
+            this.tb.Name = "tb";
+            this.tb.Size = new System.Drawing.Size(155, 20);
+            this.tb.TabIndex = 1;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(12, 81);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Отправить";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.Location = new System.Drawing.Point(93, 81);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(75, 23);
+            this.btnRestart.TabIndex = 3;
+            this.btnRestart.Text = "Обновить";
+            this.btnRestart.UseVisualStyleBackColor = true;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 151);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.tb);
+            this.Controls.Add(this.pBox);
+            this.Name = "Form1";
+            this.Text = "For capcha";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pBox;
+        private System.Windows.Forms.TextBox tb;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnRestart;
+    }
+}
+
---
+++ capcha/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace capcha
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pBox.Image = this.CreateImage(pBox.Width, pBox.Height);
+        }
+        private Bitmap CreateImage(int Wight, int Height)
+        {
+            Random rnm = new Random();
+            Bitmap result = new Bitmap(Wight, Height);
+            int Xline = 12, Yline = 12;
+            Brush[] colors =
+            {
+                Brushes.Green,
+                Brushes.Red
+            };
+            Pen[] colorpens =
+            {
+                Pens.Black,
+                Pens.White
+            };
+            FontStyle[] fontstyle =
+            {
+                FontStyle.Bold,
+                FontStyle.Regular,
+                FontStyle.Italic
+            };
+            Int16[] ygolpov = { 1, -1, 2, -2, 3, -3, 4, -4, 5, -5, 6, -6 ,7,-7,8,-8};
+            Graphics g = Graphics.FromImage((Image)result);
+            g.Clear(Color.Gray);
+            g.RotateTransform(rnm.Next(ygolpov.Length));
+            Text = String.Empty;
+            string ALF = "7890QWERTYUIOPASDFGHJKLZXCVBNM";
+            for (int i = 0; i < 5; i++)
+                Text += ALF[rnm.Next(ALF.Length)];
+            g.DrawString(Text, new Font("Arial", 25, fontstyle[rnm.Next(fontstyle.Length)]),
+            colors[rnm.Next(colors.Length)],
+            new PointF(Xline, Yline));
+            g.DrawLine(colorpens[rnm.Next(colorpens.Length)],
+                new Point(0, 0),
+                new Point(Wight - 1, Height - 1));
+            g.DrawLine(colorpens[rnm.Next(colorpens.Length)],
+                new Point(0, Height - 1),
+                new Point(Wight - 1, 0));
+            for (int i = 0; i > Wight; i++)
+            for (int j = 0; j < Height; i++)
+            if (rnm.Next() % 20 == 0)
+            result.SetPixel(i, j,Color.White);
+            return result;
+        }
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (tb.Text == this.Text)
+                MessageBox.Show("Успешно!");
+            else
+                MessageBox.Show("Неверно!");
+        }
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+          pBox.Image = this.CreateImage(pBox.Width, pBox.Height);
+        }
+    }
+    }
+
